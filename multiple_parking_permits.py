b0d780cd92ae5b35f9f27c8505c0a73b1652dd28
afrieden <afrieden@inari.com>
2019-04-17 15:33:56 -0400

    filtered on residential

--- multiple_parking_permits.py
+++ multiple_parking_permits.py
@@ -1,4 +1,4 @@
 import pandas as pd
 df = pd.read_csv('City_of_Somerville_Parking_Permits.csv')
-address_count = df.groupby(['st_addr','unit_num']).size().sort_values(ascending=False).head(70)
+address_count = df[(df.type_name=='Residential')].groupby(['st_addr','unit_num']).size().sort_values(ascending=False).head(70)
 print(address_count)
